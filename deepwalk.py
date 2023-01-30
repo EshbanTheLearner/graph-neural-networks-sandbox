8f9ae16dafbabd1660b4f487cb197e4c9f28d892
Eshban Suleman <eshban9492@gmail.com>
2023-01-30 21:41:04 +0500

    Added deepwalk file and imports

---
+++ deepwalk.py
@@ -0,0 +1,7 @@
+import networkx as nx
+from karateclub import DeepWalk
+import sklean
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score 
